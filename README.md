8dfacfdcc41c834a6b78e83f1efdf7acf98c1bf3
ChuishenX <chuishenx@oblivionocean.top>
2024-05-03 18:36:14 +0800

    Adding Zip File Support & Fixing the Timer
    As above, so below.

--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 It can be used to collect changed files in a directory during a period by scanning the changed files and copying them into a zip file in "./backup".
 
 Currently, it is used in Minecraft Servers for sensing the changing chunk files in Minecraft worlds.
+
 ## Usage
 ```shell
-python main.py [directory] [period]
+python main.py [directory] [period] [type]
 ```
 - [directory] should be a relative or absolute path for the directory you want to backup for.
-- [period] should be an integer.
+- [period] should be an integer, which is the time between each snapshot in seconds.
+- [type] should be chosen from `zip` or `tar`
+
+### Example
+
+```shell
+python main.py ./test 20 tar
+```
--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
 import sys
 import threading
 import tarfile
+import zipfile
 
 class HD(watchdog.events.FileSystemEventHandler):
-    def __init__(self, aim_path):
+    def __init__(self, aim_path, file_type, timing):
         super().__init__()
         self.timer = None
+        self.timing = timing
         self.aim_path = aim_path
         self.snapshot = DirectorySnapshot(self.aim_path)
         self.second = 120
+        self.method = tarfile.open if file_type == "tar" else zipfile.ZipFile
+        self.ext = "tar.gz" if file_type == "tar" else "zip"
 
     def checkSnapshot(self):
         snapshot = DirectorySnapshot(self.aim_path)
@@ -22,22 +26,26 @@ class HD(watchdog.events.FileSystemEventHandler):
         self.timer = None
         if not os.path.exists("./backup"):
             os.mkdir("./backup")
-        tar_file = tarfile.open('./backup/%s.tar.gz' % (time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime())),'w')
-        for x in diff.files_modified:
-            # print(x)
-            tar_file.add(x)
-        tar_file.close()
+        reducted_file = self.method('./backup/%s.%s' % (time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime()), self.ext), mode='w')
+        for x in list(diff.files_modified)+list(diff.files_created)+list(diff.files_moved):
+            if self.ext == "tar.gz":
+                reducted_file.add(x)
+                print("file %s is added" % x)
+            else:
+                reducted_file.write(x)
+                print(str(time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime()))+" file %s is added" % x)
+        reducted_file.close()
 
-    def on_modified(self, event):
+    def on_any_event(self, event):
         if self.timer:
             self.timer.cancel()
-        self.timer = threading.Timer(120, self.checkSnapshot)
+        self.timer = threading.Timer(self.timing, self.checkSnapshot)
         self.timer.start()
 
-def observe(path="", timer=120):
+def observe(path="", timer=120, file_type="tar"):
     observer = Observer()
     observer.start()
-    event_handler = HD(path)
+    event_handler = HD(path, file_type, timer)
     event_handler.second = timer
     observer.schedule(event_handler, path, recursive=True)
     try:
@@ -48,4 +56,4 @@ def observe(path="", timer=120):
     observer.join()
 
 if __name__ == '__main__':
-    observe(path=sys.argv[1], timer=int(sys.argv[2]))
+    observe(path=sys.argv[1], timer=int(sys.argv[2]), file_type=sys.argv[3])
